995a05ae7e12e87e788097868d8a3f94a9fd15d0
setbap <ebr.sina@outlook.com>
2024-07-28 18:38:13 +0330

    improve code

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,9 +3,11 @@
 --
 -- See the kickstart.nvim README for more information
 
-
 -- relative number
 vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
 
 -- Disable netrw
 vim.g.loaded_netrw = 1
@@ -16,13 +18,11 @@ vim.keymap.set('n', '<S-l>', vim.cmd.bn, { desc = 'Next Buffer' })
 vim.keymap.set('n', '<S-h>', vim.cmd.bp, { desc = 'Previous Buffer' })
 vim.keymap.set('n', '<leader>bc', vim.cmd.close, { desc = 'Close Buffer' })
 
-
 -- NOTE Folding Setting
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-
 
 vim.keymap.set('n', '<leader>st', require('telescope.builtin').colorscheme, { desc = '[S]earch [C]olorscheme' })
 
